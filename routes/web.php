6eb61514f2156eef339427ec808f40669676fa80
Timothy Birrell <tbirrell@xristos.net>
2020-02-16 15:20:32 -0600

    tos and privacy policy

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\LegalController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +11,6 @@ Route::get('login/{provider}', 'Auth\LoginController@redirectToProvider');
 Route::get('login/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/privacy', [LegalController::class, 'privacy']);
+Route::get('/tos', [LegalController::class, 'tos']);
