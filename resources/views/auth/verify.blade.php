92cb8c107e5534e4891a7abfac62ba0f730b618f
langermank <langermank@gmail.com>
2020-02-17 21:03:04 -0500

    remaining form styles

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.landing')
 
+<!-- also can't hit this blade -->
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<div class="landingWrap textCenter mg-top-lg" id="landing">
+    <h2 class="mg-btm-lg">{{ __('Verify Your Email Address') }}</h2>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
-                </div>
+        @if (session('resent'))
+            <div class="alert alert-success" role="alert">
+                {{ __('A fresh verification link has been sent to your email address.') }}
             </div>
-        </div>
+        @endif
+
+        {{ __('Before proceeding, please check your email for a verification link.') }}
+        {{ __('If you did not receive the email') }},
+        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+            @csrf
+            <button type="submit" class="button-base primary full-width mg-btm-lg">{{ __('click here to request another') }}</button>.
+        </form>
     </div>
 </div>
 @endsection
