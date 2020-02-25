e9323eafa14145533e68472d5b3621c83b6cd732
langermank <langermank@gmail.com>
2020-02-24 22:21:12 -0500

    stock card structure starts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mx-auto px-4" id="dashboard">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                    <stock-card-list>
-                    </stock-card-list>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="stock-cards">
+    <stock-card-list>
+    </stock-card-list>
+</div>
+<div class="user-details">
+    my moneys
 </div>
 @endsection
