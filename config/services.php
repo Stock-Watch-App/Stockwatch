27c6c280aeec7471b42a1c53107327bb45aaa63a
Timothy Birrell <tbirrell@xristos.net>
2020-02-26 23:09:54 -0600

    reenabled google

--- config/services.php
+++ config/services.php
@@ -50,9 +50,9 @@ return [
         'client_secret' => env('FACEBOOK_SECRET'),
         'redirect'      => env('FACEBOOK_REDIRECT_URI')
     ],
-//    'google' => [
-//        'client_id'     => env('GOOGLE_KEY'),
-//        'client_secret' => env('GOOGLE_SECRET'),
-//        'redirect'      => env('GOOGLE_REDIRECT_URI')
-//    ],
+    'google' => [
+        'client_id'     => env('GOOGLE_KEY'),
+        'client_secret' => env('GOOGLE_SECRET'),
+        'redirect'      => env('GOOGLE_REDIRECT_URI')
+    ],
 ];
