2849a0bf9a698b3492c2d1b4d3372542b2cb32f3
langermank <langermank@gmail.com>
2020-03-01 18:45:24 -0500

    img cta starts

--- resources/views/loginflow.blade.php
+++ resources/views/loginflow.blade.php
@@ -1,6 +1,11 @@
 <div class="card duo">
     <div class="form-cta">
-        <h2 class="mg-btm-lg">Katie to make sweet graphic for here</h2>
+        <h2 class="headline">A virtual stock market game for competitive reality shows</h2>
+        <div class="landing-img-wrap">
+            <picture>
+                <img src="{{ asset('/storage/landing-graph.png') }}" title="Profile image" class="landing-img" />
+            </picture>
+        </div>
     </div>
     <div class="form-wrap">
         {{ $slot }}
