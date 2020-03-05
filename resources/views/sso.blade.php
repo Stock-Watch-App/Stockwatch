9f4fa75ebc6d156a9a64c2a45fa143bef03b77df
langermank <langermank@gmail.com>
2020-03-04 21:24:49 -0500

    google sso button

--- resources/views/sso.blade.php
+++ resources/views/sso.blade.php
@@ -16,9 +16,15 @@
 </figure>
 <span>Sign-in with Discord</span>
 </a>
-<a class="button-base prepend full-width twitch icon mg-btm-lg" href="/login/twitch" alt="Sign in with Twich">
+<a class="button-base prepend full-width twitch icon mg-btm-sm" href="/login/twitch" alt="Sign in with Twich">
 <figure>
     <font-awesome-icon :icon="['fab', 'twitch']" size="lg"/>
 </figure>
 <span>Sign-in with Twitch</span>
+</a>
+<a class="button-base prepend full-width google icon mg-btm-lg" href="/login/google" alt="Sign in with Google">
+<figure>
+    <svg xmlns="http://www.w3.org/2000/svg" width="17.64" height="18" viewBox="0 0 17.64 18"><path d="M17.64,9.205a10.276,10.276,0,0,0-.164-1.841H9v3.481h4.844a4.141,4.141,0,0,1-1.8,2.716V15.82h2.908A8.779,8.779,0,0,0,17.64,9.205Z" style="fill:#4285f4;fill-rule:evenodd"/><path d="M9,18a8.594,8.594,0,0,0,5.956-2.18l-2.908-2.259A5.43,5.43,0,0,1,3.964,10.71H.957v2.332A9,9,0,0,0,9,18Z" style="fill:#34a853;fill-rule:evenodd"/><path d="M3.964,10.71a5.326,5.326,0,0,1,0-3.42V4.958H.957a9.014,9.014,0,0,0,0,8.084L3.964,10.71Z" style="fill:#fbbc05;fill-rule:evenodd"/><path d="M9,3.58a4.863,4.863,0,0,1,3.44,1.345l2.582-2.581A8.652,8.652,0,0,0,9,0,9,9,0,0,0,.957,4.958L3.964,7.29A5.364,5.364,0,0,1,9,3.58Z" style="fill:#ea4335;fill-rule:evenodd"/></svg>
+</figure>
+<span>Sign-in with Google</span>
 </a>
