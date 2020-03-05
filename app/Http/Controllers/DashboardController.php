a04167438b12ff94e745c07961d598a77aa570d2
Timothy Birrell <tbirrell@xristos.net>
2020-03-04 22:48:25 -0600

    new dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(Request $request)
+    {
+        $user = $request->user();
+
+        return view('dashboard', compact('user'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Admin\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TradeController;
 use App\Http\Controllers\LegalController;
