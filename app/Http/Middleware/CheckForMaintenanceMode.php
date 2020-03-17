df9ecbb8db03804825a3ea7951dcad818d4fd300
Timothy Birrell <tbirrell@xristos.net>
2020-03-17 14:55:04 -0500

    point maintenance mode to custom page.

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\IpUtils;
 
 class CheckForMaintenanceMode extends Middleware
 {
@@ -12,6 +15,25 @@ class CheckForMaintenanceMode extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'maintenance',
     ];
+
+    public function handle($request, Closure $next)
+    {
+        if ($this->app->isDownForMaintenance()) {
+            $data = json_decode(file_get_contents($this->app->storagePath() . '/framework/down'), true);
+
+            if (isset($data['allowed']) && IpUtils::checkIp($request->ip(), (array)$data['allowed'])) {
+                return $next($request);
+            }
+
+            if ($this->inExceptArray($request)) {
+                return $next($request);
+            }
+
+            return redirect('/maintenance');
+        }
+
+        return $next($request);
+    }
 }
