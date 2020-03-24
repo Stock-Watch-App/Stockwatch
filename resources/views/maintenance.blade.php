df6963443b2355f01f23c43e55e6ef4620b998d9
Timothy Birrell <tbirrell@xristos.net>
2020-03-24 18:29:13 -0500

    more relevent maintenance message

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -4,7 +4,7 @@
 <div class="landingWrap mg-top-lg" id="landing">
     <div class="flex-col maintenance">
         @include('logo')
-        <h3 class="mg-bottom-md">The stockwatch is unavailable right now to prepare for the roundtable. Tune in tonight at 9:00 ET to watch the LFC rate the houseguests and set prices for this week!</h3>
+        <h3 class="mg-bottom-md">The stockwatch is unavailable right now to prepare for the roundtable. Tune in tonight at 9:00 ET to watch the LFC rate the houseguests and set the final prices of the season.</h3>
         <h3>Follow <a href="https://twitter.com/ArmstrongTaran" title="Taran on Twitter" class="item-wrap" target="_blank" rel="noreferrer noopener">Taran</a> for the roundtable link</h3>
     </div>
 </div>
