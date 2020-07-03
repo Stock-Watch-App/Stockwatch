2f88dc94d550f07f42cc99a1093d737be0ddded6
Timothy Birrell <tbirrell@xristos.net>
2020-07-03 16:28:55 -0500

    slightly more apropos maintenance message

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.landing_light')
 
 @section('content')
-<div class="landingWrap mg-top-lg" id="landing">
-    <div class="flex-col maintenance">
-        @include('logo')
-        <h3 class="mg-bottom-md">The stockwatch is unavailable right now to prepare for the roundtable. Tune in tonight at 9:00 ET to watch the LFC rate the houseguests and set the final prices of the season.</h3>
-        <h3>Follow <a href="https://twitter.com/ArmstrongTaran" title="Taran on Twitter" class="item-wrap" target="_blank" rel="noreferrer noopener">Taran</a> for the roundtable link</h3>
+    <div class="landingWrap mg-top-lg" id="landing">
+        <div class="flex-col maintenance">
+            @include('logo')
+            <h3 class="mg-bottom-md">The stockwatch is undergoing maintenance right now. Thank you for your patience.</h3>
+        </div>
     </div>
-</div>
 @endsection
