a9e404c4f6caf867db913d4f363439787649356c
Timothy Birrell <tbirrell@xristos.net>
2020-07-05 13:29:11 -0500

    minor refactoring

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,30 +11,21 @@ class ProfileController extends Controller
     public function index(Request $request)
     {
         $user = $request->user();
-        $user->load('bank', 'transactions', 'leaderboard');
         $user->load([
+            'bank',
+            'transactions',
+            'leaderboard',
             'stocks' => function ($query) {
                 $query->where('quantity', '>', 0);
             }
         ]);
+        
         $season = Season::current();
 
-        $houseguests = Houseguest::where('season_id', $season->id)->withoutGlobalScope('active')->get();
-        $houseguests->load('prices');
-
-        // $bank = $user->bank;
-        // if ($bank === null) {
-        //     $stocks = $this->initGame($user);
-        //     $user->load('bank');
-        //     $bank = $user->bank;
-        // }
-
-        // $stocks = $this->getStocks($user, $season);
-
-        // $networth = $bank->money + $stocks->map(function ($stock) {
-        //         return $stock->quantity * $stock->houseguest->current_price;
-        //     })->sum();
-
+        $houseguests = Houseguest::with('prices')
+                                 ->where('season_id', $season->id)
+                                 ->withoutGlobalScope('active')
+                                 ->get();
 
         return view('profile', compact('user', 'houseguests'));
     }
