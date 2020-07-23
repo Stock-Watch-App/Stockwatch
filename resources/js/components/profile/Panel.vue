e1b584fcfa9aa3a3d9117760600cd2a283c948ee
langermank <langermank@gmail.com>
2020-07-23 07:07:49 -0400

    diff styles

--- resources/js/components/profile/Panel.vue
+++ resources/js/components/profile/Panel.vue
@@ -39,16 +39,18 @@
                             <h1>{{ networth | currency }}</h1>
                             <span
                                 v-if="networthDiff.amount >= 0"
-                                class="stock-change-wrap"
+                                class="stat-change-wrap"
                                 :class="networthDiff.class"
                             >
                                 <font-awesome-icon
-                                    v-if="networthDiff.amount - networth"
+                                    v-if="networthDiff.amount >= 0"
                                     :icon="networthDiff.icon"
                                     size="sm"
-                                    class="stock-diff-icon"
+                                    class="stat-diff-icon"
                                 />
-                                <span v-if="networthDiff.amount - networth" class="stock-diff word">{{ networthDiff.amount | currency }}</span>
+                                <span v-if="networthDiff.amount >= 0">{{
+                                    networthDiff.amount | currency
+                                }}</span>
                             </span>
                         </div>
                     </div>
@@ -58,16 +60,18 @@
                             <h1>{{ selectedRank }}</h1>
                             <span
                                 v-if="rankDiff.amount >= 0"
-                                class="stock-change-wrap"
+                                class="stat-change-wrap"
                                 :class="rankDiff.class"
                             >
                                 <font-awesome-icon
                                     v-if="rankDiff.amount - selectedRank"
                                     :icon="rankDiff.icon"
                                     size="sm"
-                                    class="stock-diff-icon"
+                                    class="stat-diff-icon"
                                 />
-                                <span v-if="rankDiff.amount - selectedRank" class="stock-diff word">{{ rankDiff.amount }}</span>
+                                <span v-if="rankDiff.amount - selectedRank">{{
+                                    rankDiff.amount
+                                }}</span>
                             </span>
                         </div>
                     </div>
@@ -86,94 +90,106 @@
 </template>
 
 <script>
-    export default {
-        props: {
-            user: Object,
-            houseguests: Array,
-            week: Number
-        },
-        data() {
-            return {
-                selectedWeek: this.week,
-                currentRank: null
-            };
-        },
-        mounted() {
-        },
-        watch: {},
-        methods: {
-            mutateRank(direction) {
-                let result =  direction === 'up' ? this.selectedWeek + 1 : this.selectedWeek - 1;
+export default {
+    props: {
+        user: Object,
+        houseguests: Array,
+        week: Number
+    },
+    data() {
+        return {
+            selectedWeek: this.week,
+            currentRank: null
+        };
+    },
+    mounted() {},
+    watch: {},
+    methods: {
+        mutateRank(direction) {
+            let result =
+                direction === "up"
+                    ? this.selectedWeek + 1
+                    : this.selectedWeek - 1;
 
-                if (this.weekSelectorOptions.find(o => o.value === result)) {
-                    this.selectedWeek = result;
-                }
-            }
-        },
-        computed: {
-            networth: function () {
-                let value = parseFloat(this.user.bank.money);
-
-                this.user.stocks.forEach(stock => {
-                    let price = parseFloat(
-                        this.houseguests
-                            .find(hg => {
-                                return hg.id === stock.houseguest_id;
-                            })
-                            .prices.find(p => {
-                            return p.week === this.week;
-                        }).price
-                    );
-
-                    value += stock.quantity * price;
-                });
-
-                return value;
-            },
-            networthDiff: function () {
-                let lastWeek = Object.assign({networth: 0}, this.user.leaderboard.find(l => {
-                    return l.week === this.selectedWeek - 1;
-                })).networth;
-
-                let diff = this.networth - lastWeek;
-                let isIncrease = diff > 0;
-
-                return {
-                    amount: Math.abs(diff),
-                    icon: isIncrease ? "arrow-up" : diff === 0 ? "" : "arrow-down",
-                    class: isIncrease ? "green" : diff === 0 ? "" : "red"
-                };
-            },
-            selectedRank: function () {
-                let rank = Object.assign({rank: 'N/A'}, this.user.leaderboard.find(l => {
-                    return l.week === this.selectedWeek;
-                })).rank;
-
-                this.currentRank = this.currentRank || rank;
-
-                return rank;
-            },
-            rankDiff: function () {
-                let lastWeek = Object.assign({rank: 0}, this.user.leaderboard.find(l => {
-                    return l.week === this.selectedWeek - 1;
-                })).rank;
-
-                let diff = (this.selectedRank === 'N/A') ? 0 : this.selectedRank - lastWeek;
-                let isIncrease = (diff > 0);
-
-                return {
-                    amount: Math.abs(diff),
-                    icon: isIncrease ? "arrow-up" : diff === 0 ? "" : "arrow-down",
-                    class: isIncrease ? "green" : diff === 0 ? "" : "red",
-                };
-            },
-            weekSelectorOptions: function () {
-                let options = [];
-                this.user.leaderboard.forEach(l => {
-                    options.push({value: l.week, text: 'Week ' + l.week})
-                });
-                return options;
+            if (this.weekSelectorOptions.find(o => o.value === result)) {
+                this.selectedWeek = result;
             }
         }
-    };
+    },
+    computed: {
+        networth: function() {
+            let value = parseFloat(this.user.bank.money);
+
+            this.user.stocks.forEach(stock => {
+                let price = parseFloat(
+                    this.houseguests
+                        .find(hg => {
+                            return hg.id === stock.houseguest_id;
+                        })
+                        .prices.find(p => {
+                            return p.week === this.week;
+                        }).price
+                );
+
+                value += stock.quantity * price;
+            });
+
+            return value;
+        },
+        networthDiff: function() {
+            let lastWeek = Object.assign(
+                { networth: 0 },
+                this.user.leaderboard.find(l => {
+                    return l.week === this.selectedWeek - 1;
+                })
+            ).networth;
+
+            let diff = this.networth - lastWeek;
+            let isIncrease = diff > 0;
+
+            return {
+                amount: Math.abs(diff),
+                icon: isIncrease ? "arrow-up" : diff === 0 ? "" : "arrow-down",
+                class: isIncrease ? "green" : diff === 0 ? "" : "red"
+            };
+        },
+        selectedRank: function() {
+            let rank = Object.assign(
+                { rank: "N/A" },
+                this.user.leaderboard.find(l => {
+                    return l.week === this.selectedWeek;
+                })
+            ).rank;
+
+            this.currentRank = this.currentRank || rank;
+
+            return rank;
+        },
+        rankDiff: function() {
+            let lastWeek = Object.assign(
+                { rank: 0 },
+                this.user.leaderboard.find(l => {
+                    return l.week === this.selectedWeek - 1;
+                })
+            ).rank;
+
+            let diff =
+                this.selectedRank === "N/A" ? 0 : this.selectedRank - lastWeek;
+            let isIncrease = diff > 0;
+
+            return {
+                amount: Math.abs(diff),
+                icon: isIncrease ? "arrow-up" : diff === 0 ? "" : "arrow-down",
+                class: isIncrease ? "green" : diff === 0 ? "" : "red"
+            };
+        },
+        weekSelectorOptions: function() {
+            let options = [];
+            this.user.leaderboard.forEach(l => {
+                options.push({ value: l.week, text: "Week " + l.week });
+            });
+            return options;
+        }
+    }
+};
 </script>
