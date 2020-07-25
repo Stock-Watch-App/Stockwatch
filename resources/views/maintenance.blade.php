d999324051b08e1662d2fdb599dff5739fb08c5a
Timothy Birrell <tbirrell@xristos.net>
2020-07-25 15:47:16 -0500

    apropos maintenance message

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -4,7 +4,7 @@
     <div class="landingWrap mg-top-lg" id="landing">
         <div class="flex-col maintenance">
             @include('logo')
-            <h3 class="mg-bottom-md">The stockwatch is undergoing maintenance right now. Thank you for your patience.</h3>
+            <h3 class="mg-bottom-md">The stockwatch is undergoing maintenance to prepare for Big Brother 22: Allstars. Thank you for your patience.</h3>
         </div>
     </div>
 @endsection
