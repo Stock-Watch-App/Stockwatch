3777acb4a4cf1fd1c2fb425213770681b3ba3745
Katie Langerman <langermank@gmail.com>
2020-08-04 18:49:02 -0400

    maintenance text

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -4,7 +4,7 @@
     <div class="landingWrap mg-top-lg" id="landing">
         <div class="flex-col maintenance">
             @include('logo')
-            <h3 class="mg-bottom-md">The stockwatch is undergoing maintenance to prepare for Big Brother 22: Allstars. Thank you for your patience.</h3>
+            <h3 class="mg-bottom-md">The stockwatch is undergoing maintenance to prepare for Big Brother 22: Allstars. We'll be going live after Wednesday's premier. Thank you for your patience!</h3>
         </div>
     </div>
 @endsection
