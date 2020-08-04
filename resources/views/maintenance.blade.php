b3ec85dad10820cec87bfeac2ed06fd23daf7d7f
Katie Langerman <langermank@gmail.com>
2020-08-04 19:03:56 -0400

    fix maintenance page

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -3,8 +3,10 @@
 @section('content')
     <div class="landingWrap mg-top-lg" id="landing">
         <div class="flex-col maintenance">
-            @include('logo')
-            <h3 class="mg-bottom-md">The stockwatch is undergoing maintenance to prepare for Big Brother 22: Allstars. We'll be going live after Wednesday's premier. Thank you for your patience!</h3>
+            <div class="logo-wrap">
+                @include('logo')
+            </div>
+            <h3 class="mg-top-md">The stockwatch is undergoing maintenance to prepare for Big Brother 22: Allstars. We'll be going live after Wednesday's premier. Thank you for your patience!</h3>
         </div>
     </div>
 @endsection
