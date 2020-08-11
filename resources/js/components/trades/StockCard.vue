62186607cda0b6f9836453b84d0a6c34ad236997
tbirrell <tbirrell@xristos.net>
2020-08-11 15:58:20 -0400

    Revert "temp HG display (revert later)"
    This reverts commit c70f58e91eb60d989c12391a3e8678bae2f256f5.

--- resources/js/components/trades/StockCard.vue
+++ resources/js/components/trades/StockCard.vue
@@ -7,74 +7,72 @@
             <h4>
                 {{
                     stock.houseguest.nickname ||
-                    stock.houseguest.first_name | capitalize
+                        stock.houseguest.first_name | capitalize
                 }}
             </h4>
         </div>
-        <img :src="houseguestImage" height="85" width="85" class="hg-img"/>
+        <img :src="houseguestImage" height="85" width="85" class="hg-img" />
         <div class="hg-rating">
             <span class="rating-wrap">
                 <h3 class="num-wrap flex-row">
-                    <font-awesome-icon icon="star" class="hg-star"/>
-                    <span class="hg-star-rating">?</span>
+                    <font-awesome-icon icon="star" class="hg-star" />
+                    <span class="hg-star-rating">{{ currentRating }}</span>
                     <span class="hg-star-outof">/10</span>
                 </h3>
             </span>
         </div>
-        <div class="hg-price" ></div>
-<!--            <span class="price-wrap">-->
-<!--        <div class="hg-price" v-bind:class="priceDifference.class.background">-->
-<!--            <span class="price-wrap">-->
-<!--                <h3>{{ currentPrice | currency }}</h3>-->
-<!--            </span> <span-->
-<!--            v-if="priceDifference.amount >= 0"-->
-<!--            class="price-change-wrap flex-row"-->
-<!--            v-bind:class="priceDifference.class.text"-->
-<!--        >-->
-<!--                <font-awesome-icon-->
-<!--                    :icon="priceDifference.icon"-->
-<!--                    class="price-diff-icon"-->
-<!--                />-->
-<!--                <p class="price-diff">-->
-<!--                    {{ priceDifference.amount | currency }}-->
-<!--                </p>-->
-<!--            </span>-->
-<!--        </div>-->
-<!--        <div v-if="stock.hasOwnProperty('quantity')" class="input-wrap">-->
-<!--            <button-->
-<!--                v-if="!disabled"-->
-<!--                class="button-base primary ghost small sell"-->
-<!--                @click="sellAll"-->
-<!--            >-->
-<!--                Sell all-->
-<!--            </button>-->
-<!--            <number-input-->
-<!--                v-model="stock.quantity"-->
-<!--                :min="0"-->
-<!--                :max="-->
-<!--                    bank.money < parseFloat(currentPrice)-->
-<!--                        ? stock.quantity-->
-<!--                        : stock.quantity + 1-->
-<!--                "-->
-<!--                controls-->
-<!--                :disabled="disabled"-->
-<!--            ></number-input>-->
-<!--            <button-->
-<!--                v-if="!disabled"-->
-<!--                class="button-base primary ghost small buy"-->
-<!--                @click="buyMax"-->
-<!--            >-->
-<!--                Buy max-->
-<!--            </button>-->
-<!--            <button-->
-<!--                v-if="!disabled"-->
-<!--                class="button-base link icon-col small"-->
-<!--                @click="reset"-->
-<!--            >-->
-<!--                <font-awesome-icon icon="undo-alt"/>-->
-<!--                reset-->
-<!--            </button>-->
-<!--        </div>-->
+        <div class="hg-price" v-bind:class="priceDifference.class.background">
+            <span class="price-wrap">
+                <h3>{{ currentPrice | currency }}</h3>
+            </span>
+            <span
+                v-if="priceDifference.amount >= 0"
+                class="price-change-wrap flex-row"
+                v-bind:class="priceDifference.class.text"
+            >
+                <font-awesome-icon
+                    :icon="priceDifference.icon"
+                    class="price-diff-icon"
+                />
+                <p class="price-diff">
+                    {{ priceDifference.amount | currency }}
+                </p>
+            </span>
+        </div>
+        <div v-if="stock.hasOwnProperty('quantity')" class="input-wrap">
+            <button
+                v-if="!disabled"
+                class="button-base primary ghost small sell"
+                @click="sellAll"
+            >
+                Sell all
+            </button>
+            <number-input
+                v-model="stock.quantity"
+                :min="0"
+                :max="
+                    bank.money < parseFloat(currentPrice)
+                        ? stock.quantity
+                        : stock.quantity + 1
+                "
+                controls
+                :disabled="disabled"
+            ></number-input>
+            <button
+                v-if="!disabled"
+                class="button-base primary ghost small buy"
+                @click="buyMax"
+            >
+                Buy max
+            </button>
+            <button
+                v-if="!disabled"
+                class="button-base link icon-col small"
+                @click="reset"
+            >
+                <font-awesome-icon icon="undo-alt" />reset
+            </button>
+        </div>
     </li>
 </template>
 
@@ -91,16 +89,16 @@ export default {
         };
     },
     methods: {
-        reset: function () {
+        reset: function() {
             this.stock.quantity = this.originalStock.quantity;
         },
-        buyMax: function () {
+        buyMax: function() {
             //this needs to be mutated from the parent because of the bank
             this.$emit("buy-max", {
                 houseguest: this.stock.houseguest_id
             });
         },
-        sellAll: function () {
+        sellAll: function() {
             //this needs to be mutated from the parent because of the bank
             this.$emit("sell-all", {
                 houseguest: this.stock.houseguest_id
@@ -108,10 +106,10 @@ export default {
         }
     },
     computed: {
-        houseguestImage: function () {
-            return "/storage/" + this.stock.houseguest.image;
+        houseguestImage: function() {
+            return "/storage" + this.stock.houseguest.image;
         },
-        currentPrice: function () {
+        currentPrice: function() {
             //find latest week
             let currentWeek;
             this.stock.houseguest.prices.forEach(week => {
@@ -130,7 +128,7 @@ export default {
 
             return currentWeek.price;
         },
-        currentRating: function () {
+        currentRating: function() {
             //find latest week
             let currentWeek = [];
             this.stock.houseguest.ratings.forEach(week => {
@@ -148,7 +146,7 @@ export default {
             });
             return Math.round(total / 4);
         },
-        priceDifference: function () {
+        priceDifference: function() {
             if (this.stock.houseguest.prices.length === 1) {
                 return {
                     amount: -1, //because we use abs(), we will never have a negative number. Thus we can use it as a check.
@@ -182,8 +180,8 @@ export default {
                     background: isIncrease
                         ? "green-bg"
                         : diff === 0
-                            ? ""
-                            : "red-bg",
+                        ? ""
+                        : "red-bg",
                     text: isIncrease ? "green" : diff === 0 ? "" : "red"
                 }
             };
