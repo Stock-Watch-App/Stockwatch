5eefbc7c130726d969472e1981ef66a75d693991
Timothy Birrell <tbirrell@xristos.net>
2020-08-12 20:28:55 -0500

    fixed market closing time banner

--- resources/views/trades.blade.php
+++ resources/views/trades.blade.php
@@ -5,7 +5,7 @@
         <h3 class="mg-btm-lg trade-heading">Buy & Sell Stocks</h3>
         <div id="market-banner" class="info flash__message">
             @if($season->status === 'open')
-                <span>The market will be closing at 8pm ET @if(date('N') === '4') today @else on Thursday @endif.</span>
+                <span>The market will be closing at 8pm ET @if(((new DateTime())->setTimezone(new DateTimeZone('America/New_York'))->format('N')) === '4') today @else on Thursday @endif.</span>
             @elseif($season->status === 'closed')
                 <span>The market is <strong>Closed</strong>. Trading will reopen following the LFC Roundtable Tuesday Evening.</span>
             @endif
