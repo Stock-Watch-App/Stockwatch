ec40f407bcfd126a035cc2de6abeaa38a5803486
Katie Langerman <langermank@gmail.com>
2020-09-14 21:33:15 -0400

    vanity tag colors

--- resources/js/components/VanityTag.vue
+++ resources/js/components/VanityTag.vue
@@ -7,16 +7,29 @@ export default {
     props: {
         name: String,
     },
-    // computed: {
-    //     color() {
-    //         switch (this.name.toLowerCase()) {
-    //             case 'lfc':
-    //                 return
-    //             default:
-    //                 return
-    //         }
-    //     }
-    // }
+    computed: {
+        color() {
+            switch (this.name.toLowerCase()) {
+                // katie's attempt, it doesn't work
+                case lfc:
+                    name.class = "lfc";
+                    break;
+                case nom:
+                    name.class = "nom";
+                    break;
+                case hoh:
+                    name.class = "hoh";
+                    break;
+                case veto:
+                    name.class = "veto";
+                    break;
+                // case 'lfc':
+                //     return
+                default:
+                    return
+            }
+        }
+    }
 }
 </script>
 
