22348938e674347e1e38135e23ec8a1b44853742
Jeff Dombroski <jdombroski@trimarkproperties.com>
2020-09-29 18:35:04 -0400

    Set slug from full name on houseguest creation

--- app/Observers/HouseguestObserver.php
+++ app/Observers/HouseguestObserver.php
@@ -3,24 +3,62 @@
 namespace App\Observers;
 
 use App\Models\Houseguest;
+use Illuminate\Support\Str;
 
 class HouseguestObserver
 {
+    public function retrieved(Houseguest $houseguest)
+    {
+    }
 
-    public function retrieved(Houseguest $houseguest){}
-    public function creating(Houseguest $houseguest){
+    public function creating(Houseguest $houseguest)
+    {
         if ($houseguest->nickname === null) {
             $houseguest->nickname = $houseguest->first_name;
         }
+
+        if ($houseguest->slug == null) {
+            $houseguest->slug = Str::slug($houseguest->name);
+        }
     }
-    public function created(Houseguest $houseguest){}
-    public function updating(Houseguest $houseguest){}
-    public function updated(Houseguest $houseguest){}
-    public function saving(Houseguest $houseguest){}
-    public function saved(Houseguest $houseguest){}
-    public function deleting(Houseguest $houseguest){}
-    public function deleted(Houseguest $houseguest){}
-    public function restoring(Houseguest $houseguest){}
-    public function restored(Houseguest $houseguest){}
-    public function forceDeleted(Houseguest $houseguest){}
+
+    public function created(Houseguest $houseguest)
+    {
+    }
+
+    public function updating(Houseguest $houseguest)
+    {
+    }
+
+    public function updated(Houseguest $houseguest)
+    {
+    }
+
+    public function saving(Houseguest $houseguest)
+    {
+    }
+
+    public function saved(Houseguest $houseguest)
+    {
+    }
+
+    public function deleting(Houseguest $houseguest)
+    {
+    }
+
+    public function deleted(Houseguest $houseguest)
+    {
+    }
+
+    public function restoring(Houseguest $houseguest)
+    {
+    }
+
+    public function restored(Houseguest $houseguest)
+    {
+    }
+
+    public function forceDeleted(Houseguest $houseguest)
+    {
+    }
 }
