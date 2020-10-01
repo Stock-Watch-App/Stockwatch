4ef3217704513010f77339b6ae51d042ff70bb1d
Timothy Birrell <tbirrell@xristos.net>
2020-10-01 14:39:57 -0500

    use nicknames

--- app/Observers/HouseguestObserver.php
+++ app/Observers/HouseguestObserver.php
@@ -7,10 +7,7 @@ use Illuminate\Support\Str;
 
 class HouseguestObserver
 {
-    public function retrieved(Houseguest $houseguest)
-    {
-    }
-
+    public function retrieved(Houseguest $houseguest){}
     public function creating(Houseguest $houseguest)
     {
         if ($houseguest->nickname === null) {
@@ -18,46 +15,19 @@ class HouseguestObserver
         }
 
         if ($houseguest->slug == null) {
-            $houseguest->slug = Str::slug($houseguest->name);
+            $houseguest->slug = Str::slug($houseguest->nickname);
         }
     }
 
-    public function created(Houseguest $houseguest)
-    {
-    }
-
-    public function updating(Houseguest $houseguest)
-    {
-    }
-
-    public function updated(Houseguest $houseguest)
-    {
-    }
-
-    public function saving(Houseguest $houseguest)
-    {
-    }
-
-    public function saved(Houseguest $houseguest)
-    {
-    }
-
-    public function deleting(Houseguest $houseguest)
-    {
-    }
-
-    public function deleted(Houseguest $houseguest)
-    {
-    }
-
-    public function restoring(Houseguest $houseguest)
-    {
-    }
-
-    public function restored(Houseguest $houseguest)
-    {
-    }
-
+    public function created(Houseguest $houseguest){}
+    public function updating(Houseguest $houseguest){}
+    public function updated(Houseguest $houseguest){}
+    public function saving(Houseguest $houseguest){}
+    public function saved(Houseguest $houseguest){}
+    public function deleting(Houseguest $houseguest){}
+    public function deleted(Houseguest $houseguest){}
+    public function restoring(Houseguest $houseguest){}
+    public function restored(Houseguest $houseguest){}
     public function forceDeleted(Houseguest $houseguest)
     {
     }
